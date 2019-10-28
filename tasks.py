d400f1e8b2e89ca6d06bb2a8b7e9c6c6694667b7
aaqib925 <muhammadaaqib925@gmail.com>
2019-10-28 20:58:25 +0500

    different challenging tasks file added

--- tasks.py
+++ tasks.py
@@ -1,9 +1,45 @@
 # program to count the number of vowels in the String
 
-sentence = input("Enter the String: ")
-vowels = 0
-for i in sentence:
-    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U":
-        vowels += 1
+# sentence = input("Enter the String: ")
+# vowels = 0
+# for i in sentence:
+#     if i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U":
+#         vowels += 1
+#
+# print("There are", vowels, "vowels in your String")
 
-print("There are", vowels, "vowels in your String")
+# Question:
+# Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+# between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+#
+# Hints:
+# Consider use range(#begin, #end) method
+
+# list1 = []
+# for i in range(2000, 3201):
+#     if (i % 7 == 0) and (i % 5 != 0):
+#         list1.append(i)
+#
+# print(list1)
+
+
+# Write a program which can compute the factorial of a given numbers.
+# The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+# =========CODE============
+
+# def fact(x):
+#     """ this function calculates the factorial of the input number"""
+#     if x == 0:
+#         return 1
+#     else:
+#         return x * fact(x - 1)
+#
+#
+# x = int(input("Enter any number: "))
+#
+# print("The factorial of", x, "is", fact(x))
