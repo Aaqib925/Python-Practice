4471253ff1d10177251c26b7816b408907d12cf6
aaqib925 <muhammadaaqib925@gmail.com>
2019-10-31 01:41:49 +0500

    Class and methods

--- class.py
+++ class.py
@@ -1,15 +1,15 @@
 # class Employee():
 
-    # making an init to make the instance for future use
-    # def __init__(self, first, last, pay):  # we are using the self as the basic argument of the init method
-    #     self.first = first
-    #     self.last = last
-    #     self.pay = pay
+# making an init to make the instance for future use
+# def __init__(self, first, last, pay):  # we are using the self as the basic argument of the init method
+#     self.first = first
+#     self.last = last
+#     self.pay = pay
 
-    # defining a method(similar to the function) in a class
+# defining a method(similar to the function) in a class
 
-    # def full_name(self):  # creating a method...argument self is necesaary
-    #     return "{} {}".format(self.first, self.last)
+# def full_name(self):  # creating a method...argument self is necesaary
+#     return "{} {}".format(self.first, self.last)
 
 
 #  making an instance for the class
@@ -33,3 +33,64 @@
 
 # print(employee_1.__dict__)
 # print(employee_2.__dict__)
+
+
+# class variables....wo variables jo k class k har instance k liye available hote hen for different modifications
+class Employee():
+    raise_amount = 1.04  # now i have to add this variable to the method where it is required
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.mail = first + last + "@company.com"
+
+    def full_name(self):
+        return self.first + self.last
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+emp_1 = Employee("Aaqib", "Nazir", 10000)
+emp_2 = Employee("Test", "User", 90000)
+
+# print(emp_1.full_name())
+# print(emp_2.full_name())
+#
+# print(emp_1.__dict__)
+# print(emp_2.__dict__)
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+#
+# print(emp_2.pay)
+# emp_2.apply_raise()
+# print(emp_2.pay)   # this is alot more diffcult...so i am gonna assign a variable class above
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+
+# emp_1.apply_raise()
+# emp_2.apply_raise()
+# print(emp_1.__dict__)
+# print(emp_2.__dict__)
+
+
+# if we want to change the raise amount number for specific employee
+
+# emp_1.raise_amount = 1.05
+# emp_1.apply_raise()
+# print(emp_1.pay)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
