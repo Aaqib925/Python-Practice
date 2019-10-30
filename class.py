ff8c33a7185e23af3b17670834c31ada235b5146
aaqib925 <muhammadaaqib925@gmail.com>
2019-10-30 18:54:23 +0500

    Class and methods

---
+++ class.py
@@ -0,0 +1,31 @@
+class Employee():
+
+    # making an init to make the instance for future use
+    def __init__(self, first, last, pay):  # we are using the self as the basic argument of the init method
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    # defining a method(similar to the function) in a class
+
+    def full_name(self):  # creating a method...argument self is necesaary
+        return "{} {}".format(self.first, self.last)
+
+
+# making an instance for the class
+employee_1 = Employee("Aaqib", "Nazir", "100k")
+employee_2 = Employee("Test", "User", "100k")
+
+# printing the attributes of the class
+
+print(employee_1.first)
+print(employee_1.last)
+print(employee_1.pay)
+# implementing the method in the instances
+print(employee_1.full_name())
+
+print(employee_2.first)
+print(employee_2.last)
+print(employee_2.pay)
+
+print(employee_2.full_name())
