e7e41c4b8046b820f615b7546be906e44a08b5e3
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-01 00:42:56 +0500

    Class and methods

---
+++ pattern assignment.py
@@ -0,0 +1,19 @@
+# using while loop to print a pattern in right triangle
+
+# row = int(input("Enter the number of columns in first line: "))
+# n = int(input("Enter the total rows of the pattern: "))
+#
+# while row < n + 1:
+#     col = 1
+#     while col <= row:
+#         print("*", end=" ")
+#         col += 1
+#
+#     print()
+#     row += 1
+
+
+# using the while loop to print the pattern allgned from left
+
+row = 1
+while row < 5
