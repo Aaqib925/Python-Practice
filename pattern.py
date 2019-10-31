1f5f125553d452a0dca88a8c0091e936c13080d3
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-01 00:23:05 +0500

    Class and methods

--- pattern.py
+++ pattern.py
@@ -63,10 +63,23 @@
 # for rows in range(10):
 #     print("*" * rows)
 
-number = int(input("Enter the total number of rows of pattern: "))
+# number = int(input("Enter the total number of rows of pattern: "))
+#
+# for i in range(1, number + 1):
+#     for j in range(1, i + 1):
+#         print("*", end=" ")
+#
+#     print("")
 
-for i in range(1, number + 1):
-    for j in range(1, i + 1):
+# for i in range(6, 1, -1):
+#     for j in range(1, i):
+#         print("*", end=" ")
+#     print()
+
+num = int(input("Enter the number of total rows in pattern: "))
+
+for i in range(num+1, 1, -1):
+    for j in range(1, i):
         print("*", end=" ")
 
-    print("")
+    print()
