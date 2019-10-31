59f587cb92f6943335e4947ac49d93fa0dd2f0c3
aaqib925 <muhammadaaqib925@gmail.com>
2019-10-31 23:19:35 +0500

    Class and methods

--- pattern.py
+++ pattern.py
@@ -11,7 +11,7 @@
 #
 # row = int(input("Enter the number of first row: "))
 #
-# n = int(input("Enter the range of the pattern: "))
+# n = int(input("Enter the number of total rows in the pattern: "))
 #
 # while row < n:     # if we put 10 as value of n then, the last pattern contains 9 stars
 #     col = 1
@@ -24,7 +24,7 @@
 
 # row = int(input("Enter the number of first row: "))
 #
-# n = int(input("Enter the range of the pattern: "))
+# n = int(input("Enter the number of total rows in the pattern: "))
 #
 # while row < n + 1:   # this will give the same number of stars in tha last line
 #     col = 1
@@ -46,15 +46,15 @@
 #     row += 1
 
 
-row = 1
-n = int(input("Enter the range of the pattern: "))
-column = int(input("Enter the no. of columns of first pattern: "))
-while row < n + 1:
-    col = column
-    while col >= row:
-        print("*", end="")
-        col -= 1
-
-    print("")
-    row += 1
+# row = 1
+# n = int(input("Enter the no. of total rows in the pattern: "))
+# column = int(input("Enter the no. of columns of first pattern: "))
+# while row < n + 1:
+#     col = column
+#     while col >= row:
+#         print("*", end="")
+#         col -= 1
+#
+#     print("")
+#     row += 1
 
