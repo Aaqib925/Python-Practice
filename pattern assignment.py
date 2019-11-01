fe26ddcb3352b85023c06d135763a7eb765201dd
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-01 16:29:33 +0500

    working

--- pattern assignment.py
+++ pattern assignment.py
@@ -43,18 +43,17 @@
 
 # code with output of straight pyramid downwards
 #
-# row = 1
-# x = " "
-# n = int(input("Enter the range of the pattern: "))
-# column = int(input("Enter the number of columns of pattern: "))
-# while row < n + 1:
-#     col = column
-#     print(x*row, end="")
-#     while col >= row:
-#         print("*", end=" ")
-#         col -= 1
-#
-#     print()
-#     row += 1
-
 row = 1
+x = " "
+n = int(input("Enter the range of the pattern: "))
+column = int(input("Enter the number of columns of pattern: "))
+while row < n + 1:
+    col = column
+    print(x*row, end="")
+    while col >= row:
+        print("*", end=" ")
+        col -= 1
+
+    print()
+    row += 1
+
