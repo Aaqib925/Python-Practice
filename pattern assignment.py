02d3dce4f7a946cb92fe045ad779b6bb2ac11b2b
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-01 16:17:48 +0500

    working

--- pattern assignment.py
+++ pattern assignment.py
@@ -6,14 +6,55 @@
 # while row < n + 1:
 #     col = 1
 #     while col <= row:
-#         print("*", end=" ")
+#         print("*", end="")
 #         col += 1
 #
 #     print()
 #     row += 1
 
 
-# using the while loop to print the pattern allgned from left
+# using the while loop to print the pattern aligned from left
+
+# row = 1
+# x = " "
+# while row < 7:
+#     col = 6
+#     print(x*row, end="")
+#     while col >= row:
+#         print("*", end="")
+#         col -= 1
+#     print()
+#     row += 1
+
+# row = 1
+# x = " "
+# n = int(input("Enter the range of the pattern: "))
+# column = int(input("Enter the number of columns of pattern: "))
+# while row < n + 1:
+#     col = column
+#     print(x*row, end="")
+#     while col >= row:
+#         print("*", end=" ")
+#         col -= 1
+#
+#     print()
+#     row += 1
+
+
+# code with output of straight pyramid downwards
+#
+# row = 1
+# x = " "
+# n = int(input("Enter the range of the pattern: "))
+# column = int(input("Enter the number of columns of pattern: "))
+# while row < n + 1:
+#     col = column
+#     print(x*row, end="")
+#     while col >= row:
+#         print("*", end=" ")
+#         col -= 1
+#
+#     print()
+#     row += 1
 
 row = 1
-while row < 5:
