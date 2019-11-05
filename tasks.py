a9ff53d7de5d34edb266a923215cfad627aca587
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-05 23:49:25 +0500

    working

--- tasks.py
+++ tasks.py
@@ -139,6 +139,12 @@
 #
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
+x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
 
+result = [[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if a + b + c != n]
+print(result)
 
 
