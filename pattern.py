936c5320ba7632b5153eb1fb4784d0759b8bf0ce
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-09 14:01:06 +0500

    working

--- pattern.py
+++ pattern.py
@@ -99,4 +99,10 @@
 #     print()
 
 
+# n = int(input("Enter the range of the pattern: "))
+# for i in range(1, n + 1):
+#
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
 
