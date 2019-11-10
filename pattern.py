c8a2a62b5e943b3e234fd410ca5d84972dfa5f21
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-10 07:38:00 +0500

    working

--- pattern.py
+++ pattern.py
@@ -121,3 +121,13 @@
 #         print(i, end="")
 #     print()
 
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return  1
+    elif n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+for n in range(1, 11):
+    print(n, ":", fibonacci(n))
