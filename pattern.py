fc290c303725a2af49ecb894d5cc40085d4e2054
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-12 20:39:29 +0500

    working

--- pattern.py
+++ pattern.py
@@ -136,20 +136,20 @@
 
 # Memoization ( to delete the cache values )
 
-from functools import lru_cache
-
-@lru_cache(maxsize= 1000)
-
-def fibonacci(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 1
-    if n > 2:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-for n in range(1, 100):
-    print(n, ":", fibonacci(n))
+# from functools import lru_cache
+#
+# @lru_cache(maxsize= 1000)
+#
+# def fibonacci(n):
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 1
+#     if n > 2:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+#
+# for n in range(1, 100):
+#     print(n, ":", fibonacci(n))
 
 
 
