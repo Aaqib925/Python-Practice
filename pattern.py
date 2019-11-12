847d2a4318d37b4672d16e55179e64c1ec3442d1
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-12 21:29:41 +0500

    working

--- pattern.py
+++ pattern.py
@@ -178,10 +178,15 @@
 #     print("*")
 
 i = 0
-j = 5
+j = 4
 for row in range(5):
     for col in range(5):
-        if row == i and col ==j:
+        if row == i and col == j:
             print("*", end="")
             i += 1
             j -= 1
+        elif row == col:
+            print("*", end='')
+        else:
+            print(end=" ")
+    print()
