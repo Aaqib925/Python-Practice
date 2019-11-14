1dc3682e79c372b27c6af21d3280082f1fd4eff7
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-14 19:39:41 +0500

    Working

--- tasks.py
+++ tasks.py
@@ -271,3 +271,7 @@
 
 # write a program to generate the square of the numbers
 
+
+n = int(input("Enter the range of the numbers: "))
+for i in range(1, n + 1):
+    print("The square of", i, "is",  i ** 2)
