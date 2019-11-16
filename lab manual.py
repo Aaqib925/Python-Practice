ce306cd87b498b7ca82e31f453ad88997f51c3e9
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-16 18:51:58 +0500

    Working

--- lab manual.py
+++ lab manual.py
@@ -38,3 +38,9 @@
 #     for j in range(i, 0, -1):
 #         print(j, end='')
 #     print()
+#
+
+# for i in range(6, 0, -1):
+#     for j in range(1, i):
+#         print(j, end="")
+#     print()
