be5f5ab1aa6d2f926cfd1cca441c4189dd288248
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-16 18:37:09 +0500

    Working

--- lab manual.py
+++ lab manual.py
@@ -32,3 +32,10 @@
 #     for j in range(1, i + 1):
 #         print(j, end="")
 #     print()
+
+n = int(input("Enter the range: "))
+for i in range(n, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end='')
+    print()
+
