21191b1b787f852ce8cb1b11466d436cdcfbea26
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-16 18:34:54 +0500

    Working

--- lab manual.py
+++ lab manual.py
@@ -9,18 +9,26 @@
 
 # fibonacci series
 
-num_terms = int(input("Enter the no. of terms on fibonacci series: "))
+# num_terms = int(input("Enter the no. of terms on fibonacci series: "))
+#
+# first_term = 1
+# second_term = 2
+#
+# counter = 0
+#
+# while counter < num_terms:
+#     print(first_term, end=", ")
+#     nth = first_term + second_term  # here second term is = nth term of iteration before
+#
+#     first_term = second_term
+#     second_term = nth
+#
+#     counter += 1
 
-first_term = 1
-second_term = 2
 
-counter = 0
-
-while counter < num_terms:
-    print(first_term, end=", ")
-    nth = first_term + second_term  # here second term is = nth term of iteration before
-
-    first_term = second_term
-    second_term = nth
-
-    counter += 1
+# n = int(input("Enter the range: "))
+# for i in range(1, n + 1):
+#
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
