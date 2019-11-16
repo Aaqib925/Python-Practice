d315826ec967e2b88c062a0d17010d6077f5db97
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-16 19:44:42 +0500

    Working

--- lab manual.py
+++ lab manual.py
@@ -47,22 +47,22 @@
 #     print()
 
 
-age = int(input("Enter the age: "))
-
-if age < 2:
-    print("The person is baby.")
-
-elif age == 2 or age < 4:
-    print("Toddler.")
-
-elif age == 4 or age < 13:
-    print("Kid.")
-
-elif age == 13 or age < 20:
-    print("Teenager.")
-
-elif age == 20 or age < 65:
-    print("Adult.")
-
-elif age >= 65:
-    print("Older.")
+# age = int(input("Enter the age: "))
+#
+# if age < 2:
+#     print("The person is baby.")
+#
+# elif age == 2 or age < 4:
+#     print("Toddler.")
+#
+# elif age == 4 or age < 13:
+#     print("Kid.")
+#
+# elif age == 13 or age < 20:
+#     print("Teenager.")
+#
+# elif age == 20 or age < 65:
+#     print("Adult.")
+#
+# elif age >= 65:
+#     print("Older.")
