86e3533b5524ff6ea0b4351d32cff6dade0ce400
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-16 19:52:58 +0500

    Working

--- lab manual.py
+++ lab manual.py
@@ -67,10 +67,19 @@
 # elif age >= 65:
 #     print("Older.")
 
-for i in range(1, 21):
-    if i == 9:
-        continue
-    elif i == 13:
-        continue
-    else:
-        print(i, end=", ")
+# for i in range(1, 21):
+#     if i == 9:
+#         continue
+#     elif i == 13:
+#         continue
+#     else:
+#         print(i, end=", ")
+
+# year = int(input("Enter the year: "))
+#
+# if year % 4 == 0:
+#     if year % 100 != 0 or year % 400 == 0:
+#         print(year, "is a leap year")
+#
+# else:
+#     print(year, "is not a leap year")
