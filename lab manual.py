828086201e5a6c735a85957f1aa1ac7f3387e802
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-16 17:58:48 +0500

    Working

--- lab manual.py
+++ lab manual.py
@@ -1,2 +1,10 @@
 # for i in range(1, 11):
 #     print(i, ",", i ** 2)
+
+# table = int(input("Enter any number: "))
+#
+# for i in range(1, 11):
+#     print(table, "*", i, "=", table * i)
+
+
+fib
