ccececc9702575238875b4af1a4061f24f07f10f
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-19 19:38:55 +0500

    Working

--- arm strong.py
+++ arm strong.py
@@ -1,10 +1,28 @@
-def counter(num):
-    length = len(str(num))
-    return length
+# def counter(num):
+#     length = len(str(num))
+#     return length
+#
+#
+# num1 = int(input("Enter any number: "))
+#
+#
+# def arm_strong():
+#     for i in range(counter(num1)):
+#         digit =
 
 
-num1 = int(input("Enter any number: "))
+# num = 333
+
+# length = len(str(num))
+
+# for items in enumerate(num):
 
 
-def arm_strong():
+# for i in range(1, length + 1):
 
+# i = 1
+# while i < length + 1:
+#     digit = str(num)
+#     dig = digit[i]
+#     print(digit)
+#     i += 1
