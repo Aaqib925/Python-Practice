1867fd041ef47442c44c5ca5c08061149e693552
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-26 22:14:40 +0500

    Working

--- tasks.py
+++ tasks.py
@@ -635,30 +635,16 @@ while length >= 0:
 
     num2 = str(num)[range1:range2]
     print(num2)
+    for i in num2:
+        if i == str(1):
 
-    for i in range(len(num2)):
-        if num2[0] == str(1):
-            total += 8
-        elif num2[0] == str(0):
-            total += 0
-        if num2[1] == str(1):
-            total += 4
-        elif num2[1] == str(0):
-            total += 0
-        if num2[2] == str(1):
-            total += 2
-        elif num2[2] == str(0):
-            total += 0
-        if num2[3] == str(1):
-            total += 1
-        elif num2[3] == str(0):
-            total += 0
+        elif i == str(0):
+            print("No")
+
     range1 += 4
     range2 += 5
     length -= 4
 print(total)
-
-
 
 
 
