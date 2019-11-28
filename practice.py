0fd5622879b35698788a5a47828608565a78ce70
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-28 16:22:14 +0500

    Working

--- practice.py
+++ practice.py
@@ -123,3 +123,15 @@
 #
 #
 # pattern1(5)
+
+# def pattern2(num):
+#     for i in range(num):
+#         for j in range(num):
+#             if i + j == num - 1:
+#                 print("*", end="")
+#             else:
+#                 print(end=" ")
+#         print()
+#
+#
+# pattern2(5)
