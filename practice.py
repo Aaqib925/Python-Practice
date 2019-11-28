0350a70095e79fa13f7a42ef1432120fc20a6701
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-28 15:37:40 +0500

    Working

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-#message = "Hello World"
+# message = "Hello World"
 # print(len(message))
 # print(message[3])
 # print(message[0:6])     this is used for the range...but the last index is exlusive this is called slicing
@@ -8,33 +8,32 @@
 # print(message.count("l"))    this counts the number of anything used in the string
 # print(message.find("World"))  this gives the index of the world we are writing in the brackets
 
-#replace function
-#message = message.replace("World", "Universe")
-#print(message)
+# replace function
+# message = message.replace("World", "Universe")
+# print(message)
 
-#adding two or more strings
+# adding two or more strings
 
 # greetings = "Hello"
 #
 # name = "world"
 
-#message = greetings + ", " + name
-#print(message)
+# message = greetings + ", " + name
+# print(message)
 
-#print(str(greetings) + ", " + str(name))
+# print(str(greetings) + ", " + str(name))
 
-#using format method of strings
+# using format method of strings
 
-#message = "{}, {}".format(greetings, name)
-#print(message)
+# message = "{}, {}".format(greetings, name)
+# print(message)
 
-#using the f strings method in python3.6 or above
+# using the f strings method in python3.6 or above
 
-#message = f"{greetings}, {name}"
-#print(message)
+# message = f"{greetings}, {name}"
+# print(message)
 
-#f strings are usefull for example
+# f strings are usefull for example
 
-#message = f"{greetings.upper()}, {name.upper()}"
-#print(message)
-
+# message = f"{greetings.upper()}, {name.upper()}"
+# print(message)
