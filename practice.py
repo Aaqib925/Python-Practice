cfedddf64617f36b36d01e1fae1d77061a97ce1d
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-28 15:52:33 +0500

    Working

--- practice.py
+++ practice.py
@@ -49,12 +49,20 @@
 #
 #     print()
 
-row = 0
-while row < 5:
-    col = 0
-    while col <= row:
-        print("*", end="")
-        col += 1
-    print()
-    row += 1
+# row = 0
+# while row < 5:
+#     col = 0
+#     while col <= row:
+#         print("*", end="")
+#         col += 1
+#     print()
+#     row += 1
 
+# row = 1
+# while row <= 5:
+#     col = 5
+#     while col >= row:
+#         print("*", end="")
+#         col -= 1
+#     print()
+#     row += 1
