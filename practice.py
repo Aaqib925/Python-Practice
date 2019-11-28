17d0e2ba40a99065afd398c5a51bc83fbbaf1cef
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-28 15:42:45 +0500

    Working

--- practice.py
+++ practice.py
@@ -42,6 +42,10 @@
 #     for j in range(1, i + 1):
 #         print("*", end="")
 #     print()
-
-
+#
+# for i in range(6, 0, -1):
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#
+#     print()
 
