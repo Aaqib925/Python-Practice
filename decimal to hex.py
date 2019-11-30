27790794a96e7a450213f92b2c510a67134c7e43
aaqib925 <muhammadaaqib925@gmail.com>
2019-11-30 13:07:21 +0500

    Working

---
+++ decimal to hex.py
@@ -0,0 +1,39 @@
+def binary(num):
+    """ Function to convert a decimal number into binary """
+
+    binary_number = ""  # in this variable...the binary number will be added after conversion
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    elif num >= 2:
+        while num > 1:
+            remainder = num % 2
+            binary_number = str(remainder) + binary_number
+            num //= 2  # this will reduce the number for the loop
+
+            if num == 1:
+                binary_number = str(1) + binary_number
+    return binary_number
+
+
+def bit(binary_num):
+    """ This function will convert the binary number into four bits """
+    len_bin_number = len(binary_num)
+    modulus = len_bin_number % 4    # this will check if the number is already in bit or not
+    bits = ""
+    if modulus != 0:
+        for b in range(modulus * 4):
+            bits = b * str(0) + str(binary_num)   # this will add zeros till the bit is closest bit is made
+            if len(bits) <= modulus * 4 and len(bits) % 4 == 0:
+                break
+    else:
+        bits = binary_num # if the number does not need any zero addition this will return the number
+
+    return bits
+
+def hexa(bits):
+    """ Fucnti"""
+
+
+
--- tasks.py
+++ tasks.py
@@ -655,68 +655,68 @@
 
 # Alhumdullilah <3
 
-print("Please think a word in your mind.")
-len_user = int(input("Enter how many letters your word has: "))
-
-print("Please enter the column number of your letter from the list below: ")
-print("""
-1\t2\t3\t4
--------------
-A\tB\tC\tD
-E\tF\tG\tH
-I\tJ\tK\tL
-M\tN\tO\tP
-Q\tR\tS\tT
-U\tV\tW\tX
-Y\tZ
-""")
-
-col_list = []
-for i in range(1, len_user + 1):
-    col = int(input("Enter the column number for {} letter of your word:".format(i)))
-    col_list.append(col)
-print(col_list)
-
-print("Now please enter the column in which your letters appear from the list below: ")
-num = "1\t2\t3\t4\t5\t6\t7"
-print(num)
-print("---------------------")
-
-last_list = []
-conj_col1 = ["A", "E", "I", "M", "Q", "U", "Y"]
-conj_col2 = ["B", "F", "J", "N", "R", "V", "Z"]
-conj_col3 = ["C", "G", "K", "O", "S", "W"]
-conj_col4 = ["D", "H", "L", "P", "T", "X"]
-for j in col_list:
-    if j == 1:
-        print("A\tE\tI\tM\tQ\tU\tY")
-        last_list.append(conj_col1)
-    if j == 2:
-        print("B\tF\tJ\tN\tR\tV\tZ")
-        last_list.append(conj_col2)
-    if j == 3:
-        print("C\tG\tK\tO\tS\tW")
-        last_list.append(conj_col3)
-    if j == 4:
-        print("D\tH\tL\tP\tT\tX")
-        last_list.append(conj_col4)
-
-final_list = []
-ls = []
-for letter_choice in range(1, len(col_list) + 1):
-    letter_choosen = int(input("Enter the column number of the {} letter from word: ".format(letter_choice)))
-    final_list.append(letter_choosen - 1)
-# print(final_list)
-
-# print(last_list)
-word = []
-for a in range(len(final_list)):
-    var = last_list[a]
-    index = final_list[a]
-    # print(index)
-    # print(var)
-    word.append(var[index])
-
-
-final_word = "".join(word)
-print("Your word is:", final_word)
+# print("Please think a word in your mind.")
+# len_user = int(input("Enter how many letters your word has: "))
+#
+# print("Please enter the column number of your letter from the list below: ")
+# print("""
+# 1\t2\t3\t4
+# -------------
+# A\tB\tC\tD
+# E\tF\tG\tH
+# I\tJ\tK\tL
+# M\tN\tO\tP
+# Q\tR\tS\tT
+# U\tV\tW\tX
+# Y\tZ
+# """)
+#
+# col_list = []
+# for i in range(1, len_user + 1):
+#     col = int(input("Enter the column number for {} letter of your word:".format(i)))
+#     col_list.append(col)
+# print(col_list)
+#
+# print("Now please enter the column in which your letters appear from the list below: ")
+# num = "1\t2\t3\t4\t5\t6\t7"
+# print(num)
+# print("---------------------")
+#
+# last_list = []
+# conj_col1 = ["A", "E", "I", "M", "Q", "U", "Y"]
+# conj_col2 = ["B", "F", "J", "N", "R", "V", "Z"]
+# conj_col3 = ["C", "G", "K", "O", "S", "W"]
+# conj_col4 = ["D", "H", "L", "P", "T", "X"]
+# for j in col_list:
+#     if j == 1:
+#         print("A\tE\tI\tM\tQ\tU\tY")
+#         last_list.append(conj_col1)
+#     if j == 2:
+#         print("B\tF\tJ\tN\tR\tV\tZ")
+#         last_list.append(conj_col2)
+#     if j == 3:
+#         print("C\tG\tK\tO\tS\tW")
+#         last_list.append(conj_col3)
+#     if j == 4:
+#         print("D\tH\tL\tP\tT\tX")
+#         last_list.append(conj_col4)
+#
+# final_list = []
+# ls = []
+# for letter_choice in range(1, len(col_list) + 1):
+#     letter_choosen = int(input("Enter the column number of the {} letter from word: ".format(letter_choice)))
+#     final_list.append(letter_choosen - 1)
+# # print(final_list)
+#
+# # print(last_list)
+# word = []
+# for a in range(len(final_list)):
+#     var = last_list[a]
+#     index = final_list[a]
+#     # print(index)
+#     # print(var)
+#     word.append(var[index])
+#
+#
+# final_word = "".join(word)
+# print("Your word is:", final_word)
