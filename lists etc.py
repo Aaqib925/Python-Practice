dfdb11c8d73f0bbe399154a31905a8f4158ff01e
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-07 15:11:01 +0500

    working

--- lists etc.py
+++ lists etc.py
@@ -205,8 +205,8 @@
 # for s in range(num_matrices):
 #     matrices = matrix(int(input("Enter the number of columns of matrix: ")), int(input("Enter the number of rows: ")))
 #     print(matrices)
-x1 = int(input("Enter the number of columns of matrix A: "))
-x2 = int(input("Enter the number of rows of matrix A: "))
+# x1 = int(input("Enter the number of columns of matrix A: "))
+# x2 = int(input("Enter the number of rows of matrix A: "))
 
 # matrix_a = matrix(x1, x2)
 #
