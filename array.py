a1017d3bc6ba1cd9288e941d61ac40f46646a8bd
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-07 19:28:49 +0500

    working

--- array.py
+++ array.py
@@ -67,11 +67,23 @@
 
 # subtracting the two matrices
 
-a = [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
-b = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# a = [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
+# b = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+# result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+#
+# for i in range(len(a)):
+#     for j in range(len(b)):
+#         result[i][j] = a[i][j] - b[i][j]
+# print(result)
+
+# multiplying two matrices
+
+a = [[21, 22, 23], [24, 25, 26], [27, 28, 29]]
+b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
 for i in range(len(a)):
     for j in range(len(b)):
-        result[i][j] = a[i][j] - b[i][j]
+        result[i][j] = a[i][j] * b[i][j]
+
 print(result)
