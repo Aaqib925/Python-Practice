b248bb9e50c157da6471f958af44838d6c9c0ed2
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-07 16:18:48 +0500

    working

--- array.py
+++ array.py
@@ -3,15 +3,23 @@
 # array is homogeneous...which mean an array can contain up to same element's data type...it can either be integer
 # float or anything else
 # for integer use int and for floating point number use float keyword
-from array import *
-my_array = array("I", [1, 2, 3])  # i means signed...and capital I means only unsigned integer
+
+
+# from array import *
+# my_array = array("I", [1, 2, 3])  # i means signed...and capital I means only unsigned integer
 # print(my_array)
 # print(my_array.buffer_info())   # this will print the address of the array and the size of the array
 # my_array.append(2)
 # my_array.remove(1)   # here the argument in remove function means the index...but it starts from 1
-my_array.pop()
-print(my_array)
+# my_array.pop()
 
+# for i in my_array:
+#     print(i)
+
+# using the characters in array use the keyword the u
+
+# my_array = array("u", ["a", "b", "c"])
+# print(my_array)
 
 
 
