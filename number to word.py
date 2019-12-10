6ce3262e3addabe36a4d237f4607534a4fd08fee
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-10 19:20:50 +0500

    working

--- number to word.py
+++ number to word.py
@@ -1,5 +1,5 @@
 dic1 = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
-dic2 = {10: "Ten", 11: "Eleven", 12: "Twelve", 13: "thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen",
+dic2 = {11: "Eleven", 12: "Twelve", 13: "thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen",
         17: "Seventeen", 18: "Eighteen", 19: "Nineteen"}
 dic3 = {10: "Ten", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty",
         90: "Ninety", 100: "Hundred"}
@@ -23,33 +23,36 @@ if length == 1:
     print(word)
 if length >= 2:
     if last == str(0) and length >= 2 and num not in dic2:
-        units = dic3.get(num)
-        word = word + str(units)
+        ten = dic3.get(num)
+        word = word + str(ten)
         if length == 2:
             print(word)
-    if length >= 2 and last == "0" and last_two not in dic3:
-        ten = dic2.get(int(last_two))
-        print(ten)
-        word = str(ten) + word
-        print(word)
-    if length >= 2 and int(last_two) in dic2:
-        tens = dic2.get(num)
-        print(tens)
+    if length >= 2 and int(last_two) in dic2 and int(last_two) not in dic3:
+
+        tens = dic2.get(int(last_two))
         word = word + str(tens)
         if length == 2 and num in dic2:
             print(word)
+    elif length >= 2 and int(last_two) in dic3 and int(last_two) not in dic2:
+        # print(num)
+        tens = dic3.get(int(last_two))
+
+        word = str(tens)
+        if length == 2 and int(last_two) in dic3:
+            print(word)
+
     elif length >= 2 and int(last_two) not in dic2 and last != str(0):
         val2 = num % 10
         val_2 = dic1.get(val2)
 
         word = word + str(" ") + str(val_2)
         num //= 10
+        if str(num)[-1] != str(0):
+            num = str(num)[-1] + str(0)
 
-        num = str(num)[-1] + str(0)
+            val__2 = dic3.get(int(num))
 
-        val__2 = dic3.get(int(num))
-
-        word = str(val__2) + word + str(" ")
+            word = str(val__2) + word + str(" ")
 
         if length == 2:
             print(word)
