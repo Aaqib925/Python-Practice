65bbc632793b7088447322f672f1f5332d69a94a
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-10 18:15:01 +0500

    working

--- number to word.py
+++ number to word.py
@@ -5,12 +5,16 @@ dic3 = {10: "Ten", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Si
         90: "Ninety", 100: "Hundred"}
 
 num = int(input("Enter any number: "))
+num_copy = num
 length = len(str(num))
+# print(length)
 last = str(num)[-1]
 last_two = str(num)[length - 2: length]
+
 # print(last)
 # print(last_two)
 
+# print(last_third)
 word = ""
 
 if length == 1:
@@ -20,7 +24,7 @@ if length == 1:
 if length >= 2:
     if last == str(0) and length >= 2 and num not in dic2:
         units = dic3.get(num)
-        word = word + units
+        word = word + str(units)
         if length == 2:
             print(word)
     if length >= 2 and int(last_two) in dic2:
@@ -31,12 +35,30 @@ if length >= 2:
     elif length >= 2 and int(last_two) not in dic2 and last != str(0):
         val2 = num % 10
         val_2 = dic1.get(val2)
+        # print(val_2)
         word = word + str(" ") + str(val_2)
         num //= 10
-        num = str(num) + str(0)
+        # print(num)
+
+        num = str(num)[1] + str(0)
+
         val__2 = dic3.get(int(num))
+        # print(val__2)
         word = str(val__2) + word + str(" ")
-        print(word)
+        # print(word)
+        if length == 2:
+            print(word)
+    if length >= 3:
+        last_third = str(num_copy)[-3]
+        if last_two == "00":
+            hundred = dic1.get(int(last_third))
+            word = str(hundred) + " Hundreds"
+            if length == 3:
+                print(word)
+        elif last_two != "00" and length >= 3:
+            hundred2 = dic1.get(int(last_third))
+            word = str(hundred2) + " Hundreds and " + word
+            print(word)
 
 
 
