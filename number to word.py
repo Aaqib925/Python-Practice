ca6e40cfdedb79cb1144fd13f26a32bdde914e3b
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-10 18:57:53 +0500

    working

--- number to word.py
+++ number to word.py
@@ -27,31 +27,35 @@ if length >= 2:
         word = word + str(units)
         if length == 2:
             print(word)
+    if length >= 2 and last == "0" and last_two not in dic3:
+        ten = dic2.get(int(last_two))
+        print(ten)
+        word = str(ten) + word
+        print(word)
     if length >= 2 and int(last_two) in dic2:
         tens = dic2.get(num)
+        print(tens)
         word = word + str(tens)
         if length == 2 and num in dic2:
             print(word)
     elif length >= 2 and int(last_two) not in dic2 and last != str(0):
         val2 = num % 10
         val_2 = dic1.get(val2)
-        # print(val_2)
+
         word = word + str(" ") + str(val_2)
         num //= 10
-        # print(num)
 
         num = str(num)[-1] + str(0)
-        # print(num)
 
         val__2 = dic3.get(int(num))
-        # print(val__2)
+
         word = str(val__2) + word + str(" ")
-        # print(word)
+
         if length == 2:
             print(word)
     if length >= 3:
         last_third = str(num_copy)[-3]
-        # print(last_third)
+
         if last_two == "00":
             hundred = dic1.get(int(last_third))
             word = str(hundred) + " Hundreds"
@@ -60,7 +64,8 @@ if length >= 2:
         elif last_two != "00" and length >= 3:
             hundred2 = dic1.get(int(last_third))
             word = str(hundred2) + " Hundreds and " + word
-            print(word)
+            if length == 3:
+                print(word)
     if length == 4:
         first = str(num_copy)[0]
         last_three = str(num_copy)[1: length]
