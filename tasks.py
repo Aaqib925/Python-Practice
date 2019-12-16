4c48ef10dc815f2ccbf2e14caee7b7a0963010f6
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-16 13:51:54 +0500

    working

--- tasks.py
+++ tasks.py
@@ -981,3 +981,18 @@
 #             list1[i] = list1[i] - list1[j]
 #
 # print("After swap", list1)
+
+list1 = []
+list_length = int(input("Enter the length for your list: "))
+for i in range(list_length):
+    x = int(input("Enter the number to add in your list: "))
+    list1.append(x)
+print("Before swap", list1)
+for i in range(len(list1)):
+    for j in range(i, len(list1)):
+        if list1[j] > list1[i]:
+            list1[i] = list1[i] + list1[j]
+            list1[j] = list1[i] - list1[j]
+            list1[i] = list1[i] - list1[j]
+
+print("After swap", list1)
