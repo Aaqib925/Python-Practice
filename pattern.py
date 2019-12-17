416f307f5e15a2ca19b9010822e8023795ca53ce
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-17 18:33:08 +0500

    working

--- pattern.py
+++ pattern.py
@@ -229,3 +229,19 @@
 #
 #
 # pattern(int(input("Enter any odd number: ")))
+
+def pattern(num):
+    if num % 2 == 0:
+        print("Please enter odd number")
+    else:
+        for i in range(num):
+            for j in range(num):
+                if i == j or (i + j == num - 1 and i < j):
+                    print("*", end="")
+                else:
+                    print(end=" ")
+
+            print()
+
+
+pattern(int(input("Enter any odd number: ")))
