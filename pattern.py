99cb81e9d34a6e53f95af0262a04cd9730f28a21
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-18 17:12:36 +0500

    working

--- pattern.py
+++ pattern.py
@@ -294,3 +294,15 @@
 #         print()
 #
 # pattern(5)
+
+def pattern(num):
+    for i in range(num):
+        for j in range(num):
+            if j == num // 2 and j < num // 2:
+                print("*", end="")
+            elif (i == j and i > num // 2) or (i + j == num - 1 and i > num // 2):
+                print("*", end="")
+            else:
+                print(end=" ")
+        print()
+
