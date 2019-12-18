8d4d868f94eba299c638c42faccdfb857406ac29
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-18 17:10:45 +0500

    working

--- pattern.py
+++ pattern.py
@@ -281,16 +281,16 @@
 # pattern(int(input("Enter any odd number: ")))
 
 # This is an tower
-def pattern(num):
-    for i in range(num):
-        for j in range(num):
-            if j == num // 2:
-                print("*", end="")
-            elif (i == j and i > num // 2) or (i + j == num - 1 and i > num // 2):
-                print("*", end="")
-            else:
-                print(end=" ")
-
-        print()
-
-pattern(5)
+# def pattern(num):
+#     for i in range(num):
+#         for j in range(num):
+#             if j == num // 2:
+#                 print("*", end="")
+#             elif (i == j and i > num // 2) or (i + j == num - 1 and i > num // 2):
+#                 print("*", end="")
+#             else:
+#                 print(end=" ")
+#
+#         print()
+#
+# pattern(5)
