78f3cac210cb88d97a7def6522608c62c498e07f
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-18 18:18:44 +0500

    working

--- OOP/classes.py
+++ OOP/classes.py
@@ -1,1 +1,4 @@
-print("Hello World")
+# class is created by using keyword class and with the name of the class
+# self is the prefix...which is called as object
+# __init__ is called as instance
+# the functions within the class are called are methods
