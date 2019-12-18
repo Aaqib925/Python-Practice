a8fc6c85c159dfe301e7db774238009e71e83d24
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-18 18:23:30 +0500

    working

--- OOP/classes.py
+++ OOP/classes.py
@@ -2,3 +2,14 @@
 # self is the prefix...which is called as object
 # __init__ is called as instance
 # the functions within the class are called are methods
+
+
+class human:
+    def __init__(self, name):
+        self.name = name
+        self.age = 10
+
+
+person_one = human("Aaqib")  # this is passing the value to the class
+print(person_one)   # this will print the memory code in which this code is saved
+print(person_one.name)  # this will print the value
