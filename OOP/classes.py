6b84597ee4b7832429ad0a5a90793d620a2619bf
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-18 18:23:42 +0500

    working

--- OOP/classes.py
+++ OOP/classes.py
@@ -4,12 +4,12 @@
 # the functions within the class are called are methods
 
 
-class human:
-    def __init__(self, name):
-        self.name = name
-        self.age = 10
-
-
-person_one = human("Aaqib")  # this is passing the value to the class
-print(person_one)   # this will print the memory code in which this code is saved
-print(person_one.name)  # this will print the value
+# class human:
+#     def __init__(self, name):
+#         self.name = name
+#         self.age = 10
+#
+#
+# person_one = human("Aaqib")  # this is passing the value to the class
+# print(person_one)   # this will print the memory code in which this code is saved
+# print(person_one.name)  # this will print the value
