56b8fc067c1d78364b512940f095378e5561cb46
aaqib925 <muhammadaaqib925@gmail.com>
2019-12-22 18:56:57 +0500

    working

--- word guesser.py
+++ word guesser.py
@@ -3,17 +3,7 @@ print("Please think of a word in your mind.")
 len_user = int(input("Enter how many letters your word has: "))
 
 print("Please enter the column number of your letter from the list below: ")
-print("""
-1\t2\t3\t4
--------------
-A\tB\tC\tD
-E\tF\tG\tH
-I\tJ\tK\tL
-M\tN\tO\tP
-Q\tR\tS\tT
-U\tV\tW\tX
-Y\tZ
-""")
+print("1\t2\t3\t4\n-------------\nA\tB\tC\tD\nE\tF\tG\tH\nI\tJ\tK\tL\nM\tN\tO\tP\nQ\tR\tS\tT\nU\tV\tW\tX\nY\tZ")
 
 col_list = []
 for i in range(1, len_user + 1):
@@ -22,9 +12,7 @@ for i in range(1, len_user + 1):
 print(col_list)
 
 print("Now please enter the column in which your letters appear from the list below: ")
-num = "1\t2\t3\t4\t5\t6\t7"
-print(num)
-print("---------------------")
+print("1\t2\t3\t4\t5\t6\t7\n-------------------------")
 
 last_list = []
 conj_col1 = ["A", "E", "I", "M", "Q", "U", "Y"]
@@ -45,28 +33,11 @@ for j in col_list:
         print("D\tH\tL\tP\tT\tX")
         last_list.append(conj_col4)
 
-final_list = []
-ls = []
-for letter_choice in range(1, len(col_list) + 1):
-    letter_choosen = int(input("Enter the column number of the {} letter from word: ".format(letter_choice)))
-    final_list.append(letter_choosen - 1)
-# print(final_list)
-
-# print(last_list)
-word = []
-for a in range(len(final_list)):
-    var = last_list[a]
-    index = final_list[a]
-    # print(index)
-    # print(var)
-    word.append(var[index])
-
-
-final_word = "".join(word)
-print("I think your word is:", final_word)
-
-# second_column = [1, 2, 4]
-# list3 = [["B", "F", "J", "N", "R", "V", "Z"], ["A", "E", "I", "M", "Q", "U", "Y"], ["B", "F", "J", "N", "R", "V", "Z"]]
-# for i in range(len(second_column)):
-#     x = list3[i][second_column[i] - 1]
-#     print(x, end="")
+tran_col = []
+for a in range(1, len_user + 1):
+    z = int(input("Enter the column number of the {} letter of word: ".format(a)))
+    tran_col.append(z - 1)
+print("Your word according to guess is: ", end="")
+for b in range(len_user):
+    letter = last_list[b][tran_col[b]]
+    print(letter, end="")
