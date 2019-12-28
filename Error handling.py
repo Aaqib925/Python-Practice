bd6643366e281974b3a2ba747a5419e4b9df28ae
aaqib <muhammadaaqib925@gmail.com>
2019-12-28 15:16:38 +0500

    working

--- Error handling.py
+++ Error handling.py
@@ -4,3 +4,11 @@
 # if the code in try block works then the except block will not be executed
 # the code in the block of finally is always executed no matter what happens
 
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    z = a / b
+    print("Your answer is: ", z)
+
+except:
+    print("Something went wrong...please try again")
