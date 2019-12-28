8819201febce1e249f8c1a76009573a6f6d8c469
aaqib <muhammadaaqib925@gmail.com>
2019-12-28 15:28:17 +0500

    working

--- Error handling.py
+++ Error handling.py
@@ -4,19 +4,30 @@
 # if the code in try block works then the except block will not be executed
 # the code in the block of finally is always executed no matter what happens
 
-try:
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-    print(a/b)
+# try:
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     print(a/b)
+#
+# except ZeroDivisionError:
+#     print("Denominator can never be zero.")
+#
+# except ValueError:
+#     print("Please enter a integer.")
+#
+# else:
+#     print("This is else block...no error found in the try block")
+#
+# finally:
+#     print("This is finally block....I am gonna run what ever it is..huhu")
 
-except ZeroDivisionError:
-    print("Denominator can never be zero.")
 
-except ValueError:
-    print("Please enter a integer.")
+# we can also raise an exception by using th keyword raise with the exception
 
-else:
-    print("This is else block...no error found in the try block")
+# x = int(input("Enter any positive number: "))
 
-finally:
-    print("This is finally block....I am gonna run what ever it is..huhu")
+# if x < 0:
+#     raise Exception("Please enter positive number.")  # but this is going to print the exception like error
+# else:
+#     print(x)
+
