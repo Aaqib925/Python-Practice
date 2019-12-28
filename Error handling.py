dd857f64ee4f9461513fcbc5417ca622c7376f09
aaqib <muhammadaaqib925@gmail.com>
2019-12-28 15:15:19 +0500

    working

--- Error handling.py
+++ Error handling.py
@@ -1,4 +1,6 @@
 # there are 4 keywords use for the error handling in the code
 # try, except, else and finally
 # if the code in the try block fails then it goes to the expect block....and then to finally...
-# if the
+# if the code in try block works then the except block will not be executed
+# the code in the block of finally is always executed no matter what happens
+
