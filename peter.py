318291ae56daba79120883d5b5abbfc5c92c1d55
aaqib <muhammadaaqib925@gmail.com>
2019-12-29 19:01:18 +0500

    working

--- peter.py
+++ peter.py
@@ -67,60 +67,60 @@
 #
 # print(ans)
 
-from msvcrt import getch
-import sys
-import time
-
-string = "Please predict the answer                  "
-print("Enter the petition: ", end="")
-sys.stdout.flush()
-answer = ""
-counter = 0
-for i in range(len(string)):
-    char = getch()
-    if ord(char.decode()) == 13:    # Enter ASCII code to break the petition
-        break
-    elif ord(char.decode()) == 8:
-        counter -= 1
-        print("\b \b", end="")
-        sys.stdout.flush()
-    else:
-        counter += 1
-        if char.decode() != "/":
-            print(char.decode(), end="")   # Printing what user is typing
-            sys.stdout.flush()
-
-        elif char.decode() == "/":
-            print(string[counter - 1], end="")
-            sys.stdout.flush()
-
-            while char.decode() == "/":
-                z = getch()
-                counter += 1
-                print(string[counter - 1], end="")    # replacing the letters
-                sys.stdout.flush()
-                if z.decode() == "/":
-                    break
-                else:
-                    answer += z.decode()
-
-print()
-print()
-default_string = input("Enter your question: ")
-print()
-y = "Thinking..."
-for i in y:
-    time.sleep(0.1)
-    print(i, end="")
-    sys.stdout.flush()
-
-print()
-if len(answer) == 0:
-    print("I only answer my developer...Sorry")
-if answer == "no":
-    print("Sorry, I am learning, can't answer this.")
-elif len(answer) != 0:
-    print("The answer according to prediction is: ", answer)
+# from msvcrt import getch
+# import sys
+# import time
+#
+# string = "Please predict the answer                  "
+# print("Enter the petition: ", end="")
+# sys.stdout.flush()
+# answer = ""
+# counter = 0
+# for i in range(len(string)):
+#     char = getch()
+#     if ord(char.decode()) == 13:    # Enter ASCII code to break the petition
+#         break
+#     elif ord(char.decode()) == 8:
+#         counter -= 1
+#         print("\b \b", end="")
+#         sys.stdout.flush()
+#     else:
+#         counter += 1
+#         if char.decode() != "/":
+#             print(char.decode(), end="")   # Printing what user is typing
+#             sys.stdout.flush()
+#
+#         elif char.decode() == "/":
+#             print(string[counter - 1], end="")
+#             sys.stdout.flush()
+#
+#             while char.decode() == "/":
+#                 z = getch()
+#                 counter += 1
+#                 print(string[counter - 1], end="")    # replacing the letters
+#                 sys.stdout.flush()
+#                 if z.decode() == "/":
+#                     break
+#                 else:
+#                     answer += z.decode()
+#
+# print()
+# print()
+# default_string = input("Enter your question: ")
+# print()
+# y = "Thinking..."
+# for i in y:
+#     time.sleep(0.1)
+#     print(i, end="")
+#     sys.stdout.flush()
+#
+# print()
+# if len(answer) == 0:
+#     print("I only answer my developer...Sorry")
+# if answer == "no":
+#     print("Sorry, I am learning, can't answer this.")
+# elif len(answer) != 0:
+#     print("The answer according to prediction is: ", answer)
 
 
 
