2bb3fee6925fe861eb9a2410dc1d439b0fcc4189
aaqib <muhammadaaqib925@gmail.com>
2019-12-29 19:20:10 +0500

    working

--- tasks.py
+++ tasks.py
@@ -1180,3 +1180,10 @@ except ZeroDivisionError:
     print("Cannot divide by zero", end="")
 else:
     print("Division successful", end="")
+
+try:
+    data = data / 5
+except:
+    print("Inside except block", end="")
+else:
+    print("GFG", end="")
