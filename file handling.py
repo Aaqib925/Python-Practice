12a6d4a1d0fea2ca432ce4f3fa6703b837797edd
aaqib <muhammadaaqib925@gmail.com>
2019-12-30 00:42:05 +0500

    working

--- file handling.py
+++ file handling.py
@@ -10,57 +10,54 @@
 
 # now i will create a file by using open keyword...but by using the mode...else it will show me error of file not found
 
-f = open("text.txt")
-print(f)
+# f = open("text.txt")
+# print(f)
 
-f = open("text.txt", "r")  # this will create a file...
-print(f.read())
-print(f.readline(), end="")
-print(f.readline(), end="")
+# f = open("text.txt", "r")  # this will create a file...
+# print(f.read())
+# print(f.readline(), end="")
+# print(f.readline(), end="")
 
-print(f.readlines())  # this will print all the lines...and then shows in the list
+# print(f.readlines())  # this will print all the lines...and then shows in the list
 
-for x in f:
-    print(x)
+# for x in f:
+#     print(x)
 
-f = open("text.txt", "a")
-f.write("Hello")
-
-f = open("text.txt", "w")    # write will overwrite all the contents where as append will add in the last of the file
-f.write("Aaqib")
-f = open("text.txt", "r")
-print(f.readlines())
+# f = open("text.txt", "a")
+# f.write("Hello")
+#
+# f = open("text.txt", "w")    # write will overwrite all the contents where as append will add in the last of the file
+# f.write("Aaqib")
+# f = open("text.txt", "r")
+# print(f.readlines())
 
 # if you want to open a image file ...we use mode of rb...which means read binary....but will show us the hexa code
 # which are the pixels of that image file
 
 # you also delete a file my importing the os and then delete file
 
-import os
-os.remove("text.txt")  # this will delete the file
+# import os
+# os.remove("text.txt")  # this will delete the file
 
 # we can delete the directory by using the syntax os.rmdir() and passing the directory name..but the
 # directory must be empty
 
 # lab manual exercise
 
-f = open("employee.txt", "a")
+# f = open("employee.txt", "a")
+#
+# emplo_id = input("Enter the id of employee: ")
+# emplo_name = input("Enter the name of employee: ")
+# emplo_salary = input("Enter salary of employee: ")
+#
+# f.write(emplo_id)
+# f.write("\n")
+# f.write(emplo_name)
+# f.write("\n")
+# f.write(emplo_salary)
+# print("\n")
 
-emplo_id = input("Enter the id of employee: ")
-emplo_name = input("Enter the name of employee: ")
-emplo_salary = input("Enter salary of employee: ")
-
-f.write(emplo_id)
-f.write("\n")
-f.write(emplo_name)
-f.write("\n")
-f.write(emplo_salary)
-print("\n")
-
-f = open("employee.txt", "r")
-print(f.read(), end="")
-
-f.close()
-
-import os
-os.remove("employee.txt")
+# f = open("employee.txt", "r")
+# print(f.read(), end="")
+#
+# f.close()
