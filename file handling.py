7bd7222737a9eecec6d8bf6acd7f0766d1720558
aaqib <muhammadaaqib925@gmail.com>
2019-12-29 23:35:09 +0500

    working

---
+++ file handling.py
@@ -0,0 +1,11 @@
+# we can create our files in the same directory and access them in our code by using some code
+# to open any file mn use open() function and to close we use close() along with the variable of that file
+# my open the file my using the open function...but there also methods by which we can change the mode in which we
+# wanna open our file...like "r" for read mode, "a" for append mode, "w" for write mode, "t" for text mode
+# for reading the lines of the file we use read()...in the parenthesis we can pass the integer value which is the
+# range of how many lines we wanna print..to read one whole line we use readline() and for reading all lines we use
+# readlines....for writing or adding lines in our particular file we use the mode as append...and for writing into
+# file we can use the write function
+
+
+# now i will create a file by using open keyword...but by using the mode...else it will show me error of file not found
