9aa8b5c96695b0b994f95e82e72a22a38b3b817c
aaqib <muhammadaaqib925@gmail.com>
2020-01-17 14:06:34 +0500

    working

--- tasks.py
+++ tasks.py
@@ -2089,7 +2089,7 @@ for i in range(num):
     for j in range(num):
         if i == 0 or j == 0 or i == num - 1 or j == num - 1:
             print("*", end="")
-        elif i == j or i + j == num - 1:
+        elif i == j or i + j == num - 1 and (i != num // 2 and j != num // 2):
             print("*", end="")
         elif i == num // 2:
             print("*", end="")
