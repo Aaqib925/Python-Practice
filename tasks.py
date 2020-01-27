62c5b98149fa6ea60a0ccf0939dd3c51c4005ff6
aaqib <muhammadaaqib925@gmail.com>
2020-01-27 19:57:39 +0500

    working

--- tasks.py
+++ tasks.py
@@ -2720,16 +2720,16 @@
 # print(count)
 
 # ls = [["1", "1", "1", "2"], ["1", "9", "1", "2"], ["1", '6', "7", "2"], ["1", "2", "3", "4"]]
-ls = [["9", "8", "9"], ["1", "9", "1"], ["1", "1", "1"]]
-for i in range(1, len(ls) - 1):
-    for j in range(1, len(ls[0]) - 1):
-        x = ls[i][j]
-        if x > ls[i - 1][j] and x > ls[i][j - 1] and x > ls[i][j + 1] and x > ls[i + 1][j]:
-            temp = ls[i][j]
-            temp = list(temp)
-            temp[0] = "X"
-            temp1 = ""
-            for a in temp:
-                temp1 += a
-            ls[i][j] = temp1
-print(ls)
+# ls = [["9", "8", "9"], ["1", "9", "1"], ["1", "1", "1"]]
+# for i in range(1, len(ls) - 1):
+#     for j in range(1, len(ls[0]) - 1):
+#         x = ls[i][j]
+#         if x > ls[i - 1][j] and x > ls[i][j - 1] and x > ls[i][j + 1] and x > ls[i + 1][j]:
+#             temp = ls[i][j]
+#             temp = list(temp)
+#             temp[0] = "X"
+#             temp1 = ""
+#             for a in temp:
+#                 temp1 += a
+#             ls[i][j] = temp1
+# print(ls)
