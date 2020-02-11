7fabbdf1c9e3058e0e2a2e8b1d930c1f92b460ae
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-11 15:16:11 +0500

    working

--- tasks.py
+++ tasks.py
@@ -3125,3 +3125,14 @@
 # new = "0." + x[2:]
 # print(new)
 # print("big" > "small")
+# number = 10
+# if number > 11:
+#   print(0)
+# elif number != 10:
+#   print(1)
+# elif number >= 20 or number < 12:
+#   print(2)
+# else:
+#   print(3)
+# print(9999 + 8888)
+# print(100 * 100)
