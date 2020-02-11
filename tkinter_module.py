ca14b5efbb763708805faf73ce94c52076207a82
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-11 19:38:44 +0500

    working

--- tkinter_module.py
+++ tkinter_module.py
@@ -11,29 +11,49 @@ from tkinter import *
 # # to hold the screen we use the mainloop fucntion
 # root.mainloop()
 
+# root = Tk()
+# # this is the upper frame
+# topframe = Frame(root)
+#
+# topframe.pack()
+#
+# # for the lower frame we will create one and pass the parameter in pack function
+# bottomframe = Frame(root)
+# bottomframe.pack(side=BOTTOM)
+#
+# # for creating buttons for each frame
+# # fg means foreground, which is not compulsory
+# button1 = Button(topframe, text="Button1", fg="Blue")
+# button2 = Button(topframe, text="Button2", fg="red")
+# button3 = Button(bottomframe, text="Button3", fg="green")
+# button4 = Button(bottomframe, text="Button4", fg="brown")
+#
+# # by default they are made on the top of other
+#
+# button1.pack(side=LEFT)
+# button2.pack(side=RIGHT)
+# button3.pack(side=LEFT)
+# button4.pack(side=RIGHT)
+#
+# root.mainloop()
+
+# for the widgets
+
 root = Tk()
-# this is the upper frame
-topframe = Frame(root)
 
-topframe.pack()
+# bg means background and fg means foreground which means color of font
+# we will use fill paramter to make the labels grow inside the main window
+# fill = X means left and right and fill = Y means up and down
 
-# for the lower frame we will create one and pass the parameter in pack function
-bottomframe = Frame(root)
-bottomframe.pack(side=BOTTOM)
+# first label
 
-# for creating buttons for each frame
-# fg means foreground, which is not compulsory
-button1 = Button(topframe, text="Button1", fg="Blue")
-button2 = Button(topframe, text="Button2", fg="red")
-button3 = Button(bottomframe, text="Button3", fg="green")
-button4 = Button(bottomframe, text="Button4", fg="brown")
+label1 = Label(root, text="One", bg="Pink", fg="Blue")
+label1.pack(side=TOP, fill=X)
 
-# by default they are made on the top of other
+label2 = Label(root, text="Two", bg="Black", fg="white")
+label2.pack(side=LEFT, fill=Y)
 
-button1.pack(side=LEFT)
-button2.pack(side=RIGHT)
-button3.pack(side=LEFT)
-button4.pack(side=RIGHT)
+label3 = Label(root, text="Three", bg="White", fg="Blue")
+label3.pack(side=RIGHT, fill=X)
 
 root.mainloop()
-
