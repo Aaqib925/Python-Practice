605154eb51f11d46432fb1b1ef9d7dcbc96d4dc3
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-11 16:52:38 +0500

    working

--- tasks.py
+++ tasks.py
@@ -3199,3 +3199,22 @@
 #   return result
 #
 # print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
+
+def combine_guests(guests1, guests2):
+    # Combine both dictionaries into one, with each key listed
+    # only once, and the value from guests1 taking precedence
+    new = {}
+    for i in guests1:
+        new[i] = guests1[i]
+    for j in guests2:
+        if j in new:
+            new[j] += guests2[j]
+        else:
+            new[j] = guests2[j]
+    return new
+
+
+Rorys_guests = {"Adam": 2, "Brenda": 3, "David": 1, "Jose": 3, "Charlotte": 2, "Terry": 1, "Robert": 4}
+Taylors_guests = {"David": 4, "Nancy": 1, "Robert": 2, "Adam": 1, "Samantha": 3, "Chris": 5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
