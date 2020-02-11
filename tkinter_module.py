efb1f521fcc66b0fb1ad16d77bb5355ac3c9a077
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-11 19:23:29 +0500

    working

--- tkinter_module.py
+++ tkinter_module.py
@@ -21,5 +21,16 @@ topframe.pack()
 bottomframe = Frame(root)
 bottomframe.pack(side=BOTTOM)
 
+# for creating buttons for each frame
+# fg means foreground, which is not compulsory
+button1 = Button(topframe, text="Button1", fg="Blue")
+button2 = Button(topframe, text="Button2", fg="red")
+button3 = Button(bottomframe, text="Button3", fg="green")
+button4 = Button(bottomframe, text="Button4", fg="brown")
+button1.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+
 root.mainloop()
 
