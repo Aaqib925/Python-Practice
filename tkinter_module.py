de9a3b4494e9d056dfb871c12ae0a15596b866c4
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-11 19:07:30 +0500

    working

--- tkinter_module.py
+++ tkinter_module.py
@@ -1,12 +1,13 @@
 from tkinter import *
 
-root = Tk()
-# label is anything to be written or widget
-mylabel = Label(root, text="Hello Aaqib \nHow are you doing\nAbbas is a sneke")
-# now placing the label on the window
+# root = Tk()
+# # label is anything to be written or widget
+# mylabel = Label(root, text="Hello Aaqib \nHow are you doing")
+# # now placing the label on the window
+#
+# # pack function is going to place the label where it gets place by itself
+# mylabel.pack()
+#
+# # to hold the screen we use the mainloop fucntion
+# root.mainloop()
 
-# pack function is going to place the label where it gets place by itself
-mylabel.pack()
-
-# to hold the screen we use the mainloop fucntion
-root.mainloop()
