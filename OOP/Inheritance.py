6c5527861c195a1c809a88a0ff7d45d8d5196247
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-12 13:20:11 +0500

    working

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -1,20 +1,21 @@
 # Inheritance is the technique to get all the attributes of the parent class into the child class
 # This is done by passing the parameters in the child class of the parent class
 
-class Fruit:
-    def __init__(self, color, flavor):
-        self.color = color
-        self.flavour = flavor
+# class Fruit:
+#     def __init__(self, color, flavor):
+#         self.color = color
+#         self.flavour = flavor
 
 
-class Apple(Fruit):  # inherited from the parent class of Fruits
-    pass
+# class Apple(Fruit):  # inherited from the parent class of Fruits
+#     pass
 
-class Bannana(Fruit):  # inherited from the parent class of Fruits
-    pass
+# class Bannana(Fruit):  # inherited from the parent class of Fruits
+#     pass
 
 
-apple1 = Apple("Red", "Sweet")
-banana = Bannana("White", "Very Sweet")
+# apple1 = Apple("Red", "Sweet")
+# banana = Bannana("White", "Very Sweet")
 
-print(apple1.color, banana.flavour)
+# print(apple1.color, banana.flavour)
+
