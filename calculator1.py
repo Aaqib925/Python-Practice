8308c5258b381be450195aab6fc1c4c41ab43da3
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-13 17:46:05 +0500

    working

--- calculator1.py
+++ calculator1.py
@@ -5,7 +5,7 @@ root.title("Simple Calculator")
 
 # creating an entry box
 
-entry_box = Entry(root, width=41, borderwidth=5)
+entry_box = Entry(root, width=41, borderwidth=5, bg="grey", fg="White")
 entry_box.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 def button_click(number):
@@ -71,16 +71,16 @@ def button_equal():
         entry_box.insert(0, f_num * second_num)
 
 
-button1 = Button(root, text="1", padx=35, pady=15, command=lambda: button_click(1))
-button2 = Button(root, text="2", padx=35, pady=15, command=lambda: button_click(2))
-button3 = Button(root, text="3", padx=35, pady=15, command=lambda: button_click(3))
-button4 = Button(root, text="4", padx=35, pady=15, command=lambda: button_click(4))
-button5 = Button(root, text="5", padx=35, pady=15, command=lambda: button_click(5))
-button6 = Button(root, text="6", padx=35, pady=15, command=lambda: button_click(6))
-button7 = Button(root, text="7", padx=35, pady=15, command=lambda: button_click(7))
-button8 = Button(root, text="8", padx=35, pady=15, command=lambda: button_click(8))
-button9 = Button(root, text="9", padx=35, pady=15, command=lambda: button_click(9))
-button0 = Button(root, text="0", padx=35, pady=15, command=lambda: button_click(0))
+button1 = Button(root, text="1", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(1))
+button2 = Button(root, text="2", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(2))
+button3 = Button(root, text="3", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(3))
+button4 = Button(root, text="4", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(4))
+button5 = Button(root, text="5", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(5))
+button6 = Button(root, text="6", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(6))
+button7 = Button(root, text="7", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(7))
+button8 = Button(root, text="8", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(8))
+button9 = Button(root, text="9", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(9))
+button0 = Button(root, text="0", padx=35, pady=15, bg="grey", fg="White", command=lambda: button_click(0))
 buttonequal = Button(root, text="=", padx=34, pady=15, command=button_equal)
 buttonadd = Button(root, text="+", padx=42, pady=15, command=button_add)
 buttonsub = Button(root, text="-", padx=44, pady=15, command=button_sub)
