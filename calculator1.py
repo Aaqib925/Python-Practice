dde5620ee3aafae7d7697874fc4c834394239158
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-13 15:46:56 +0500

    working

--- calculator1.py
+++ calculator1.py
@@ -12,15 +12,17 @@ def button_click():
     return
 
 
-button1 = Button(root, text="1", command=button_click)
-button2 = Button(root, text="2", command=button_click)
-button3 = Button(root, text="3", command=button_click)
-button4 = Button(root, text="4", command=button_click)
-button5 = Button(root, text="5", command=button_click)
-button6 = Button(root, text="6", command=button_click)
-button7 = Button(root, text="7", command=button_click)
-button8 = Button(root, text="8", command=button_click)
-button9 = Button(root, text="9", command=button_click)
-button0 = Button(root, text="0", command=button_click)
+button1 = Button(root, text="1", padx=40, pady=40, command=button_click)
+button2 = Button(root, text="2", padx=40, pady=40, command=button_click)
+button3 = Button(root, text="3", padx=40, pady=40, command=button_click)
+button4 = Button(root, text="4", padx=40, pady=40, command=button_click)
+button5 = Button(root, text="5", padx=40, pady=40, command=button_click)
+button6 = Button(root, text="6", padx=40, pady=40, command=button_click)
+button7 = Button(root, text="7", padx=40, pady=40, command=button_click)
+button8 = Button(root, text="8", padx=40, pady=40, command=button_click)
+button9 = Button(root, text="9", padx=40, pady=40, command=button_click)
+button0 = Button(root, text="0", padx=40, pady=40, command=button_click)
+
+
 
 root.mainloop()
