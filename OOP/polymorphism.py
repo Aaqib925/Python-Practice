23eac339520aed2fcbaddcce71061ab5728c6440
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-15 17:06:23 +0500

    working

---
+++ OOP/polymorphism.py
@@ -0,0 +1,10 @@
+# polymorphism is the ability in OOP to use common interface for different data types
+
+class Parrot:
+    def fly(self):
+        print("Parrots can fly")
+
+    def swim(self):
+        print("Parrots can't swim")
+
+class Penguin:
