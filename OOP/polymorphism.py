975c2388a73ea3ee57e9c33570fc6ba268510820
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-15 17:09:17 +0500

    working

--- OOP/polymorphism.py
+++ OOP/polymorphism.py
@@ -7,6 +7,7 @@ class Parrot:
     def swim(self):
         print("Parrots can't swim")
 
+
 class Penguin:
     def fly(self):
         print("Penguins can't fly")
@@ -19,10 +20,19 @@ class Penguin:
 parrot = Parrot()
 penguin = Penguin()
 
+
 def flying_test(bird):
     bird.fly()
+
+
+def swim_test(bird):
+    bird.swim()
+
 
 # passing the instances into the flying test functions
 
 flying_test(parrot)
 flying_test(penguin)
+
+swim_test(parrot)
+swim_test(penguin)
