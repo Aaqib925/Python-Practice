82f8ce5a0e31dbea7b56b310c382ca84cbbb1a9e
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-28 15:23:23 +0500

    working

--- OOP/property decorators.py
+++ OOP/property decorators.py
@@ -1,23 +1,23 @@
 # creating a class
 
-# class Employee:
-#     def __init__(self, first, last):
-#         self.first = first
-#         self.last = last
-#
-#     @property
-#     def fullname(self):
-#         return "{} {}".format(self.first, self.last)
-#
-#     @property
-#     def email(self):
-#         return "{}.{}@company.com".format(self.first, self.last)
-#
-#
-# empl1 = Employee("Aaqib", "Nazir")
-# empl1.first = "Hello"
-# print(empl1.fullname)
-# print(empl1.email)
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
+
+    @property
+    def email(self):
+        return "{}.{}@company.com".format(self.first, self.last)
+
+
+empl1 = Employee("Aaqib", "Nazir")
+empl1.first = "Hello"
+print(empl1.fullname)
+print(empl1.email)
 
 # getter setters and deleters
 
