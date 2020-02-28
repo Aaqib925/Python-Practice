bd97c6d60ef6fc4685c38e7c0d35ea3925e4e38c
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-28 15:04:22 +0500

    working

--- OOP/Dunders.py
+++ OOP/Dunders.py
@@ -12,7 +12,13 @@ class Employee:
     def __repr__(self):
         return "Employee ('{}', '{}', '{}')".format(self.first, self.last, self.pay)
 
+    # by using the dunders we can specify which function will do what for the class objects
+    def __add__(self, other):
+        return self.pay + other.pay
+
 
 empl1 = Employee("Aaqib", "Nazir", 100000)
+empl2 = Employee("test", "user", 100000)
 print(empl1.__repr__())
 print(empl1.__str__())
+print(empl1 + empl2)
