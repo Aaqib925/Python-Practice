3525e80c2779e5b6ffce8b5d2d7c6ff60ff76bee
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-28 15:16:43 +0500

    working

--- OOP/property decorators.py
+++ OOP/property decorators.py
@@ -4,3 +4,16 @@ class Employee:
     def __init__(self, first, last):
         self.first = first
         self.last = last
+
+    @property
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
+
+    @property
+    def email(self):
+        return "{}.{}@company.com".format(self.first, self.last)
+
+
+empl1 = Employee("Aaqib", "Nazir")
+print(empl1.fullname)
+print(empl1.email)
