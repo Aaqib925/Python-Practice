81592159fb93cc0a1c481ad284fabab9ce359e1b
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-02 01:27:22 +0500

    working

--- tasks.py
+++ tasks.py
@@ -3820,9 +3820,9 @@
 x = input("Enter any word: ")
 cond = True
 
-for i in range(len(x)):
-    print(i, -(i + 1))
 
+for i in range(len(x)):
+    print(x[i], x[-(i + 1)])
 # matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 
 # matrix = [[1, 2, 3, 4], [7, 8, 9, 10], [13, 14, 15, 16], [19, 20, 21, 22], [25, 26, 27, 28]]
