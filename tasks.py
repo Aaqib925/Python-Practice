8daa90590cf7419ee5daaa3a7add9abad2ed084d
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-05 20:09:48 +0500

    working

--- tasks.py
+++ tasks.py
@@ -3846,7 +3846,6 @@ matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 #             print(matrix[i][j])
 #     print("Done")
 
-
 layer = 0
 columns = len(matrix[0]) - 1
 length = len(matrix) - 1
