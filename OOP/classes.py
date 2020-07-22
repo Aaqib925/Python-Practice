ee69bed953bb77b2ed8a0db38fed4c2bf1fb910c
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-22 15:49:19 +0500

    working

--- OOP/classes.py
+++ OOP/classes.py
@@ -197,3 +197,19 @@ empl_2 = Employee("Test", "User", 100)
 # mydate = datetime.date(2020, 2, 15)
 # print(Employee.isworkday(mydate))
 
+class Car:
+    def __init__(self, speed, unit):
+        self.speed = speed
+        self.unit = unit
+
+    def __str__(self):
+        return "Car with the maximum speed of {} {}".format(self.speed, self.unit)
+
+
+class Boat:
+    def __init__(self, speed):
+        self.speed = speed
+
+    def __str__(self):
+        return "Boat with the maximum speed of {} knots".format(self.speed)
+
